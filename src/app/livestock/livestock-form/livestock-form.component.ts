0685c4849174d171bbb5623ac0789bec38207cfd
Ricks-Pennington <k.ricks-pennington@accenturefederal.com>
2020-02-14 16:23:05 -0600

    changed livestock form to reactive form

--- src/app/livestock/livestock-form/livestock-form.component.ts
+++ src/app/livestock/livestock-form/livestock-form.component.ts
@@ -1,3 +1,5 @@
+import { FormGroup } from "@angular/forms";
+import { FormBuilder } from "@angular/forms";
 import { Aquarium } from "./../../aquarium/aquarium.model";
 import { Router, ActivatedRoute, Params } from "@angular/router";
 import { Component, OnInit, ViewEncapsulation, Input } from "@angular/core";
@@ -13,13 +15,15 @@ import { Observable } from "rxjs";
 export class LivestockFormComponent implements OnInit {
   livestocks: Observable<Livestock[]>;
   livestock: Livestock = new Livestock();
+  form: FormGroup;
   submitted = false;
   private options = ["Male", "Female", "N/A"];
 
   constructor(
     private service: LivestockService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private fb: FormBuilder
   ) {}
 
   ngOnInit() {
@@ -28,30 +32,42 @@ export class LivestockFormComponent implements OnInit {
       this.service.loadAllLivestock(fkAquariumId);
       let test = parseInt(fkAquariumId);
       console.log("value " + fkAquariumId);
-      this.livestock = {
-        livestockId: null,
-        name: "",
-        species: "",
-        gender: "",
-        notes: "",
-        fkAquariumId: test
-      };
+      // this.livestock = {
+      //   livestockId: null,
+      //   name: "",
+      //   species: "",
+      //   gender: "",
+      //   notes: "",
+      //   fkAquariumId: test
+      // };
+      this.createForm(test);
     });
   }
 
-  onAddLivestock(fkAquariumId) {
+  createForm(param) {
+    this.form = this.fb.group({
+      livestockId: null,
+      name: "",
+      species: "",
+      gender: "",
+      notes: "",
+      fkAquariumId: param
+    });
+  }
+
+  onAddLivestock() {
     this.saveLivestock();
     this.service.getAllLivestock();
   }
 
   saveLivestock() {
-    this.service.createLivestock(this.livestock).subscribe(data => {
+    this.service.createLivestock(this.form.value).subscribe(data => {
       this.ngOnInit();
       console.log(data);
+    }, error => {
+      console.log(error);
     });
-    this.livestock = new Livestock();
   }
 
   //onUpdate() {}
-  
 }

2ffee39f051d6aa35575c9f0e5e222090745cfae
Ricks-Pennington <k.ricks-pennington@accenturefederal.com>
2020-03-12 13:37:22 -0500

    file uploading properly. progress bar now implemented

--- src/app/modules/image-upload/image-form/image-form.component.ts
+++ src/app/modules/image-upload/image-form/image-form.component.ts
@@ -1,21 +1,32 @@
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
-import { ImageHttpService } from 'src/app/core/services/image-http.service';
-import { Image } from 'src/app/modules/image-upload/image.model';
-import { AngularFireStorage } from '@angular/fire/storage';
+import { FormBuilder, FormGroup } from "@angular/forms";
+import { Component, OnInit } from "@angular/core";
+import { ImageHttpService } from "src/app/core/services/image-http.service";
+import { Image } from "src/app/modules/image-upload/image.model";
+import {
+  AngularFireStorage,
+  AngularFireUploadTask,
+  AngularFireStorageReference
+} from "@angular/fire/storage";
+import { Observable } from "rxjs";
+import { finalize, map } from "rxjs/operators";
 
 @Component({
-  selector: 'app-image-form',
-  templateUrl: './image-form.component.html',
-  styleUrls: ['./image-form.component.scss']
+  selector: "app-image-form",
+  templateUrl: "./image-form.component.html",
+  styleUrls: ["./image-form.component.scss"]
 })
 export class ImageFormComponent implements OnInit {
   form: FormGroup;
-  imgSrc = '../../../../assets/images/placeholder.png';
+  imgSrc = "../../../../assets/images/placeholder.png";
   selectedFile = null;
   image: Image = new Image();
+  uploadProgress: Observable<any>;
+  downloadURL: Observable<any>;
+  task: AngularFireUploadTask;
+  message;
+  ref: AngularFireStorageReference;
 
-  constructor(private fb: FormBuilder, private service: ImageHttpService, private storage: AngularFireStorage) {}
+  constructor(private fb: FormBuilder, private storage: AngularFireStorage) {}
 
   ngOnInit() {
     this.createForm();
@@ -24,8 +35,8 @@ export class ImageFormComponent implements OnInit {
   createForm() {
     this.form = this.fb.group({
       image: null,
-      name: '',
-      category: ''
+      name: "",
+      category: ""
     });
     this.image = this.form.value;
   }
@@ -34,73 +45,36 @@ export class ImageFormComponent implements OnInit {
     const file = event.target.files[0];
     if (file) {
       const reader = new FileReader();
-      reader.onload = () =>
-      this.imgSrc = reader.result as string;
+      reader.onload = () => (this.imgSrc = reader.result as string);
       reader.readAsDataURL(file);
       this.selectedFile = file;
     }
   }
 
-  onSubmit(form) {
-    console.log(form);
-    console.log(this.image);
+  upload(form) {
     const filePath = `${form.category}/${form.name}`;
-    console.log(this.form.value);
-    this.storage.upload(filePath, this.selectedFile);
+    this.ref = this.storage.ref(filePath);
+    this.task = this.ref.put(this.selectedFile);
+    this.uploadProgress = this.task.percentageChanges();
+
+    this.task
+      .snapshotChanges()
+      .pipe(finalize(() => (this.downloadURL = this.ref.getDownloadURL())))
+      .subscribe( data => {
+        this.message = ((data.bytesTransferred / data.totalBytes) * 100);
+      });
+
+    // this.task = this.storage.upload(filePath, this.selectedFile);
+    // this.message = this.task
+    //   .snapshotChanges()
+    //   .pipe(map(s => (s.bytesTransferred / s.totalBytes) * 100));
+    // this.transfer = this.message.bytesTransferred;
+    // this.total = this.message.totalBytes;
+
+    this.createForm();
   }
 
+  onSubmit(form) {
+    this.upload(form);
+  }
 }
-
-  // public onFileSelected(event) {
-  //   this.selectedFile = event.target.files[0];
-  //   console.log(this.selectedFile);
-  //   console.log(event);
-  // }
-
-  // onUpload() {
-  //   const fd = new FormData();
-  //   fd.append('image', this.selectedFile, this.selectedFile.name);
-
-  //   this.service.upload(fd).subscribe(data => {
-  //     if (data.status === 200) {
-  //       this.message = 'Image uploaded successfully';
-  //     } else {
-  //       this.message = 'Image not uploaded successfully';
-  //     }
-  //   });
-  // }
-
-  // readImage(image: Blob) {
-  //   const reader = new FileReader();
-  //   reader.readAsDataURL(image);
-  //   reader.onloadend = () => {
-  //     this.base64Data = reader.result;
-  //     console.log(this.base64Data);
-  //     this.showImage = this.sanitizer.bypassSecurityTrustResourceUrl(
-  //       'data:image/jpeg;base64,' + this.base64Data
-  //     );
-  //   };
-
-    // reader.addEventListener("load", () => {
-    //   this.imageUrl = reader.result;
-    //   this.showImage = this.sanitizer.bypassSecurityTrustUrl(this.imageUrl);
-    //   console.log(this.showImage);
-    // }, false);
-
-    // if(image) {
-    //   reader.readAsText(image);
-    // }
-
-  // Gets called when the user clicks on retieve image button to get the image from back end
-  // getImage() {
-  //   this.service.getImage(this.imageName).subscribe(
-  //     data => {
-  //       this.response = data;
-  //       this.base64Data = this.response.image;
-  //       this.showImage = this.sanitizer.bypassSecurityTrustUrl('data:image/jpeg;base64,' + this.base64Data);
-  //     },
-  //     error => {
-  //       console.log(error);
-  //     }
-  //   );
-  // }

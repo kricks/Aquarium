f56de950bb97a951373f422f87b2a297707284cd
Ricks-Pennington <k.a.ricks-pennington@accenturefederal.com>
2020-05-20 14:42:51 -0500

    changed styling for dashboard. colors of data points, added dark header to logs

--- src/app/modules/aquarium-dashboard/parameter/chart/chart.component.ts
+++ src/app/modules/aquarium-dashboard/parameter/chart/chart.component.ts
@@ -19,18 +19,17 @@ export class ChartComponent implements OnInit {
   legend = true;
   showLabels = true;
   animations = true;
-  view = [950, 500];
   curve = shape.curveCardinal.tension(.25);
   xAxis = true;
   yAxis = true;
   showYAxisLabel = true;
   showXAxisLabel = true;
-  xAxisLabel = 'date';
-  yAxisLabel = 'ppm';
-  timeline = true;
+  xAxisLabel = 'Date';
+  yAxisLabel = 'PPM';
 
   colorScheme = {
-    domain: ['#5AA454', '#E44D25', '#CFC0BB', '#7aa3e5', '#a8385d', '#aae3f5'],
+    domain: ['#1BBC9B', '#2A80B9', '#E77E23', '#9B58B5', '#4FC3F7',
+    '#F1C40F', '#C1392B', '#ECF0F1', '#ABBCF7', '#B51A62' ],
   };
 
   constructor(private service: HttpParameterService) {}
